5ca7e71b4d82388d2c3701043ac988ea863f4d39
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-09-29 20:22:38 +0530

    Add files via upload

---
+++ double_decor.py
@@ -0,0 +1,11 @@
+def decor(fun):
+    def inner():
+        result = fun()
+        return result*2
+    return inner 
+
+def num():
+    return 4
+
+result_decor=decor(num)
+print(result_decor())
---
+++ double_thelist_map.py
@@ -0,0 +1,5 @@
+lst = [1,2,3,4,5,6,7,8,9]
+
+result=list(map(lambda x:x*2,lst))
+print(result)
+
