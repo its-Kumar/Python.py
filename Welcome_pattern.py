822d54eb106279422d6514125505fad68848c202
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-10-29 19:33:40 +0530

    Add files via upload

---
+++ Welcome_pattern.py
@@ -0,0 +1,17 @@
+"""
+WAP to implement the given pattern:
+
+ Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+"""
+
+n,m = map(int,input().split())
+upper = [(".|." * (2*i +1)).center(m,'-') for i in range(n//2)]
+lower = upper[::-1]
+print ("\n".join(upper+["WELCOME".center(m,'-')]+lower))
