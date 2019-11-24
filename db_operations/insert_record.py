8b94e71efc6ddb0fb673e142dd2e7599edcbcc0a
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-11-24 15:32:19 +0530

    Update insert_record.py

--- db_operations/insert_record.py
+++ db_operations/insert_record.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conn = mysql.connector.connect(host="localhost", database="mydb", user="root", password='mysql')
+conn = mysql.connector.connect(host="localhost", database="mydb", user="root", password='###')
 
 cur = conn.cursor()
 sql = "INSERT INTO EMPLOYEE (FIRST_NAME,LAST_NAME,AGE,SEX,INCOME)"\
