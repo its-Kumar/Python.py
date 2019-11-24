4050bc6a6263f4f5e7ea755be7a600df2842a46a
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-11-24 15:37:43 +0530

    Update use_of_WHERE.py

--- db_operations/use_of_WHERE.py
+++ db_operations/use_of_WHERE.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conn = mysql.connector.connect(host="localhost", database="mydb", user="root", password='mysql')
+conn = mysql.connector.connect(host="localhost", database="mydb", user="root", password='####')
 cur = conn.cursor()
 cur.execute("SELECT FIRST_NAME,LAST_NAME,AGE FROM EMPLOYEE WHERE FIRST_NAME='KUMAR'")
 
