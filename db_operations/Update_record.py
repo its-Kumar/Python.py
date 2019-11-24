64bc829bb68526145761ed53e87d94d207fabda7
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-11-24 15:27:00 +0530

    Update Update_record.py

--- db_operations/Update_record.py
+++ db_operations/Update_record.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conn = mysql.connector.connect(host="localhost", database="mydb", user="root", password='mysql')
+conn = mysql.connector.connect(host="localhost", database="mydb", user="root", password='####')
 
 cur = conn.cursor()
 sql = "UPDATE employee SET AGE=21 WHERE FIRST_NAME = 'kumar'"
